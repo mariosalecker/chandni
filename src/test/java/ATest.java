11011906fd4ef0c9a470eed40e5271744bf8e22a
chandni <chandni.nietcs@gmail.com>
2021-07-12 14:43:02 +0530

    added a new testcase

--- src/test/java/ATest.java
+++ src/test/java/ATest.java
@@ -1,8 +1,17 @@
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
 
 import static org.mockito.Mockito.*;
 
+
+
+
 public class ATest {
+	
+	//@InjectMocks
+	//private A classAObj;
 
     @Test
     public void testProcessRequest() {
@@ -10,5 +19,16 @@ public class ATest {
                 .useConstructor().defaultAnswer(CALLS_REAL_METHODS));
         doReturn(new Token()).when(spy).getToken();
         spy.processRequest();
+    }
+    
+    @Test
+    public void testProcessRequest_chandni() {
+    	A myAObject = new A();
+    	
+    	 A myInstance = new A();
+    	 A mySpy = Mockito.spy(myInstance);
+    	 Mockito.doReturn(new Token()).when(mySpy).getToken();
+    	 myAObject.processRequest();
+    	 
     }
 }
